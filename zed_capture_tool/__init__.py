423158ed849720d031a4a00a67c071f3f73f699b
Behnam Karimi <disorian@gmail.com>
2025-03-02 11:12:24 +1100

    feat: restructure project structure

---
+++ zed_capture_tool/__init__.py
@@ -0,0 +1,8 @@
+"""
+ZED Camera Capture Tool
+
+A Python application for capturing images from a ZED camera
+with options for time interval or GPS location-based capture.
+"""
+
+__version__ = '0.1.0'
